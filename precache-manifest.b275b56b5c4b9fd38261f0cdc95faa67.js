9128f7c43fadb1b058a6f7f213dd655de29c2f6e
Soheil Tehrani <s.tehrani1994@gmail.com>
2020-07-05 02:19:28 +0430

    Updates

---
+++ precache-manifest.b275b56b5c4b9fd38261f0cdc95faa67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c34fce2511a3e01d0c24fdb18a0eac",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "0a7c610f95c6ed101189",
+    "url": "/face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "929686c3b78e13f8f062",
+    "url": "/face-detection/static/css/main.ce487732.chunk.css"
+  },
+  {
+    "revision": "0a7c610f95c6ed101189",
+    "url": "/face-detection/static/js/2.85cfc230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detection/static/js/2.85cfc230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929686c3b78e13f8f062",
+    "url": "/face-detection/static/js/main.92ee959e.chunk.js"
+  },
+  {
+    "revision": "9874f1600706cf3d424c",
+    "url": "/face-detection/static/js/runtime-main.a66e1575.js"
+  }
+]);
